79e348aef77023530c00594a313df4f2e06c5fe1
sooof <sooof@me.com>
2020-05-14 14:51:22 -0700

    test

---
+++ GoLearnEveryday/golang/gin/bootstrapvue-demo/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "github.com/gin-gonic/gin"
+)
+
+func main(){
+  r := gin.Default()
+  r.LoadHTMLGlob("dist/*.html")    // 添加入口index.html
+  r.LoadHTMLFiles("css/*/*")	// 添加资源路径
+  r.Static("/css", "./dist/css") 	// 添加资源路径
+  r.LoadHTMLFiles("js/*/*")	// 添加资源路径
+  r.Static("/js", "./dist/js") 	// 添加资源路径
+  r.LoadHTMLFiles("img/*/*")	// 添加资源路径
+  r.Static("/img", "./dist/img") 	// 添加资源路径
+  r.StaticFile("/hello/", "dist/index.html")  //前端接口
+
+  r.Run(":8080")
+}
